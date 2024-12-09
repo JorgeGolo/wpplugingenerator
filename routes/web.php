9aefe2cc60ae96b40f7ebf268f29cb40a88a45f4
Jorge Gómez López <jorgelogomez@gmail.com>
2024-12-09 12:47:04 +0100

    initial routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,35 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/*
 Route::get('/', function () {
-    /*return view('welcome');*/
+    //return view('welcome');
 return "Bienvenido a mi proyecto de Laravel";
 });
+*/
+
+Route::get('/', HomeController::class);
 
 
 Route::get('plugins', function () {
     return "Bienvenido a la página de plugins";
 });
 
-/*
+
 Route::get('plugins/create', function () {
     return "Inserción de formulario";
 });
-*/
 
 Route::get('plugins/{plugin}', function ($plugin) {
     return "Url con la variable $plugin";
-});
-
-Route::get('otrascosas/{subruta}/{otrasubruta}', function($subruta, $otrasubruta) {
-    return "Estamos en la ruta $subruta y en la subruta $otrasubruta";
-});
-
-Route::get('nuevaruta/{subruta}/{otrasubruta?}', function($subruta, $otrasubruta = null) {
-    if ($otrasubruta) {
-        return "Estamos en la ruta $subruta y en la subruta $otrasubruta";
-    }
-    else {
-        return "Estamos en la ruta $subruta";
-    }
 });
