bd436b7c155965c28bdfb7d94c5452410820e5f1
Jorge Gómez López <jorgelogomez@gmail.com>
2024-12-09 13:20:35 +0100

    Using Views 2

--- app/Http/Controllers/PluginController.php
+++ app/Http/Controllers/PluginController.php
@@ -8,14 +8,23 @@ use Illuminate\Http\Request;
 class PLuginController extends Controller
 {
     public function index(){
-        return "Bienvenido a la página de cursos";
+       // return "Bienvenido a la página de cursos";
+       return view("plugins.index");
     }
 
     public function create(){
-        return "Inserción de formulario";
+       return view("plugins.create");
     }
 
-    public function show($curso){
-        return "Url con la variable $curso";
+    public function show($plugin){
+        //return "Url con la variable $curso";
+        
+        // definición de la variable
+        //return view("plugins.show", ['plugin' => $plugin]);
+
+        // Cuando tenemos intención de pasarle una variable a la vista que coincide con el nombre
+        return view("plugins.show", compact("plugin"));
+
     }
+
 }
